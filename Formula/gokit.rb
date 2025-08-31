587f29423a6ee5a3e51fc0ada00f09c1125f465a
GitHub Action <action@github.com>
2025-08-31 11:30:33 -0700

    Update GoKit to v0.2.5

--- Formula/gokit.rb
+++ Formula/gokit.rb
@@ -1,7 +1,7 @@
 class Gokit < Formula
   desc "GoKit - Microservice development toolkit"
   homepage "https://github.com/kumarabd/gokit"
-  version "0.2.4"
+  version "0.2.5"
 
   on_macos do
     if Hardware::CPU.arm?
