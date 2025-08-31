71df7dadb262f786c5b111f82274856e849df105
GitHub Action <action@github.com>
2025-08-31 11:38:14 -0700

    Update GoKit v1.0.0 with real SHA256 hashes

--- Formula/gokit.rb
+++ Formula/gokit.rb
@@ -6,16 +6,16 @@ class Gokit < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/kumarabd/gokit/releases/download/v#{version}/gokit-darwin-arm64"
-      sha256 "PLACEHOLDER_SHA256_ARM64"
+      sha256 "0ba9b97ed8a5ca1716bab5de46bc8eab63f424ab136c59825a180af6059a3fb7"
     else
       url "https://github.com/kumarabd/gokit/releases/download/v#{version}/gokit-darwin-amd64"
-      sha256 "PLACEHOLDER_SHA256_AMD64"
+      sha256 "03e0f0df809ab25f9b9aa4e2afdd69c0bd4eede96dcde87a9b1980d61d35961c"
     end
   end
 
   on_linux do
     url "https://github.com/kumarabd/gokit/releases/download/v#{version}/gokit-linux-amd64"
-    sha256 "PLACEHOLDER_SHA256_LINUX"
+    sha256 "6e187fdbe9663cababb6becc9127eff58bde911f3d1a45e5d780251d936701a3"
   end
 
   def install
